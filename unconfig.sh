3fe90a32f16b9ca4615528a0bb5cfc9b831a961b
Adam Gough <a@adamgough.ca>
2016-03-24 17:37:36 -0400

    Add vm unconfiguration script

---
+++ unconfig.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# check if we are running as root
+if [ `id -u` -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
+
+# delete ssh keys
+/bin/rm -f /etc/ssh/ssh_host_*
+
+# delete log files
+/bin/rm -f /var/log/*
+
+# clear bash history
+/bin/rm -f /root/.bash_history
+/bin/rm -f /home/adam/.bash_history
+
+# shutdown
+shutdown -h now
